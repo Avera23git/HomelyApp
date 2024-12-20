9474875ebbac5055c2af7dc2052f5083541473d3
Chika Jennifer Adehor <jenniferadeghor@gmail.com>
2024-12-20 15:41:38 +0100

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors(
     {
-        origin: 'https://homely-properties-ks9x.vercel.app',
+        origin: ['http://localhost:5173', 'https://homely-properties-ks9x.vercel.app'],
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
         Credential: true,
     }
