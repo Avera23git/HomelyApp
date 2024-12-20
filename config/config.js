97bcaf961a38cf0e3c4bb0af148472b9d2874b8f
Chika Jennifer Adehor <jenniferadeghor@gmail.com>
2024-12-20 12:52:22 +0100

    Update config.js

--- config/config.js
+++ config/config.js
@@ -2,13 +2,14 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-        dbName: 'homelyapp'
-    }
-    );
+    // to ensure the URI is correctly referenced from the environment variables
+    await mongoose.connect(process.env.MONGODB_URI, {
+      
+      dbName: 'homelyapp' 
+    });
     console.log("MongoDB connected...");
   } catch (err) {
-    console.error('error connecting to MongoDb', err.message );
+    console.error('error connecting to MongoDb', err.message);
     process.exit(1); // Exit process with failure
   }
 };
